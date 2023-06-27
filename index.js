2ceb9fb6bc90455d88462d1516e74b685790a9ae
Diebut <guevaradiego_2011@hotmail.com>
2023-06-27 14:30:21 -0300

    cargo codigo de consola en el index

---
+++ index.js
@@ -0,0 +1,30 @@
+//import './objects-introduccion/methods.js
+console.log(typeof "hello world")
+console.log( typeof 100)
+console.log(typeof false)
+
+console.log(typeof [])
+console.log(typeof {})
+
+console.log(typeof new Date() )
+console.log(typeof new RegExp() )
+
+console.log( new Number(100) )
+console.log( new Boolean(true) )
+console.log( new String("Diegol") )
+
+let name = "Diegol"
+let lastname = "Gol"
+let edad = 38
+
+const user ={
+    name: "Diegol",
+    lastname: "Gol",
+    edad: 38,
+    hobbies: ['Run', 'Programmers', 'Gym'],
+    address:{
+        street:"New York",
+        city: "London"
+    }
+}
+console.log(user)
