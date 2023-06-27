3e6b45185c9a5080e2ae3836fe4976777c58ef39
Diebut <guevaradiego_2011@hotmail.com>
2023-06-27 19:43:45 -0300

    se crea un archivo 6 New

---
+++ paso6-New/object.js
@@ -0,0 +1,10 @@
+const mankind = {
+    Person: function(params) {
+     "use strict"
+     this.name = ""
+     this.lastname = ""
+    }
+ }
+  const person = new mankind.Person()
+  console.log(person)
+  console.log(mankind)
