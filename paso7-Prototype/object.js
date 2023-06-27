791aa417200689831d3a1b6a3ba5705e41d1f337
Diebut <guevaradiego_2011@hotmail.com>
2023-06-27 19:46:11 -0300

    se crea carpeta 7 Prototipos

---
+++ paso7-Prototype/object.js
@@ -0,0 +1,35 @@
+function Person(name, lastname) {
+    this.name = name
+    this.lastname= lastname
+    this.displayName = function name(params) {
+        return `${this.name} ${this.lastname}` 
+    }
+}
+const jhon = new Person('John', 'Block')
+const Pedro = new Person('Pedro', 'Rossi')
+const maria = new Person('maria', 'perez')
+const jose = new Person('jose', 'efe')
+
+
+
+Person.prototype.saludar = function (params) {
+    return `Hola mi Nombre es: ${this.name}`
+}
+
+Person.prototype.age = 100
+
+console.log(jose.age)
+console.log(maria)
+console.log(Pedro)
+console.log(jhon)
+
+const s = new String("Hello world")
+
+String.prototype.concatTest = function(params) {
+    return this + " test ";
+}
+
+console.log("My name is Jesus".concatTest()) // es lo mismo
+console.log(s.concatTest())
+
+//el prototype hace refencia a un contructor
