0e6b6c5f6cde37fd7e4e5d17906c1c84ff9b3fcc
Diebut <guevaradiego_2011@hotmail.com>
2023-06-27 21:21:37 -0300

    se crea archivo composicion

---
+++ paso9-Principios de OOP/Composicion.js
@@ -0,0 +1,10 @@
+const person ={
+    name:'pepe',
+    lastname: 'soria',
+    address: {
+        street: '123 baker street',
+        city: 'london',
+        country: 'united kingdom'
+
+    }
+}
